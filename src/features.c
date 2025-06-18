53f1cc67d26a8c1f6cc50b6a94b9f2067b0a6a6f
florian-lg <florian.legrand@etu.estia.fr>
2025-06-18 18:43:10 +0200

    first_pixel

--- src/features.c
+++ src/features.c
@@ -14,3 +14,22 @@
 void helloWorld() {
     printf("Hello World !");
 }
+void first_pixel(char *source_path) {
+    unsigned char *data;
+    int width, height, channels; 
+    int result = read_image_data(source_path, &data, &width, &height, &channels); 
+    if (result != 0) {
+        printf("Error reading image\n");
+        return;
+    }
+    if (channels < 3) {
+        printf("Need at least 3 channels\n");
+        free(data);
+        return;
+    }
+    int r = data[0];
+    int g = data[1];
+    int b = data[2];
+    printf("first_pixel: %d, %d, %d\n", r, g, b);
+    free(data);
+}
